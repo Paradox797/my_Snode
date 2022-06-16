218aaa18a80bb1e0a45ad4877f65b403ecaad57f
swadkhan <swadkhan5384@gmail.com>
2022-06-16 07:08:44 +0600

    added sets

---
+++ _sets.js
@@ -0,0 +1,23 @@
+/**
+ * Basic server setup
+ * create a folder(manually or mkdir)
+ * open command line to that folder
+ * npm init
+ * npn install express
+ * create index.js
+ *
+ * for express/Node server
+ * 1.require express
+ * create app variable
+ * declear port
+ * set app.get('/')
+ * listen to port
+ * node index.js
+ * check browser for that port
+ * 
+ * get
+ * ---------
+ * 1.cors middleware
+ * 
+ * post
+ */
